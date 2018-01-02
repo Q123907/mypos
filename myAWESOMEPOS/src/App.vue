8674866a15488a4f515ae91eb5ecede8b4073aa3
Q123907 <1239077364@qq.com>
2018-01-02 20:57:05 +0800

    add mypos

---
+++ myAWESOMEPOS/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <left-nav></left-nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import leftNav from './components/leftNav'
+export default {
+  name: 'app',
+  components:{
+    leftNav,
+  }
+}
+</script>
+
+<style>
+
+</style>
